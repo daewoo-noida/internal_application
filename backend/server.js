a7850b7d4dc27e74ed4cf5c08f4e582cc5c15a53
daewoo-noida <help@daewooappliances.in>
2025-12-04 12:47:55 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ app.get('/health', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
